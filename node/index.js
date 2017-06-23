7e086ce7dac1acae48c224e38e50b5032971fed7
Ada V <oxiggy@awsmtek.com>
2017-06-23 23:10:29 +0300

    начало работы над серверной частью

---
+++ node/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const DISCORD_TOKEN= process.env.DISCORD_TOKEN;
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+    if (msg.content === 'ping') {
+        msg.reply('Pong!');
+    }
+});
+
+client.login(DISCORD_TOKEN);
