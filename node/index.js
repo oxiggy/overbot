573ebc44600e61cb936f38b249a656aaa2412473
Ada V <oxiggy@awsmtek.com>
2017-06-23 23:59:02 +0300

    начало работы над браузерной частью

--- node/index.js
+++ node/index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+const manifest= require('../package.json');
+
+
+
+// DISCORD CLIENT
+
 const DISCORD_TOKEN= process.env.DISCORD_TOKEN;
 
 const Discord = require('discord.js');
@@ -16,3 +22,31 @@ client.on('message', msg => {
 });
 
 client.login(DISCORD_TOKEN);
+
+
+
+// HTTP SERVER
+
+const HTTP_PORT= process.env.HTTP_PORT;
+
+const App= require('koa')
+const AppLogger= require('koa-logger')
+const AppStatic= require('koa-static')
+const AppRouter= require('koa-router')
+
+const app = new App;
+const api = new AppRouter;
+
+app.use(AppStatic('./browser'));
+
+app.use(AppLogger());
+
+api.get('/api/v1', async (context) => {
+    context.body= {
+        version: manifest.version,
+    };
+});
+
+app.use(api.routes());
+
+app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}!`));
