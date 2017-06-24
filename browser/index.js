f10e305283176d229d5f0da2051d16140b5e5ab4
Ada V <oxiggy@awsmtek.com>
2017-06-24 16:54:25 +0300

    Информация о версии

--- browser/index.js
+++ browser/index.js
@@ -1,2 +1,6 @@
 new Discord(document.getElementById('discord'));
 new Process(document.getElementById('process'));
+
+fetch('/api/v1').then(res => res.json()).then((data) => {
+    document.getElementById('version').innerHTML= `v${data.version}`;
+})
