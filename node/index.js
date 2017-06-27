a9c920e6212b715568d917dd8dcf54d59474db6a
Ada V <oxiggy@awsmtek.com>
2017-06-28 00:45:03 +0300

    Бот отвечает на приветствие и показывает статистику игрока

--- node/index.js
+++ node/index.js
@@ -15,9 +15,15 @@ discord.on('ready', () => {
     console.log(`Logged in as ${discord.user.tag}!`);
 });
 
-discord.on('message', msg => {
-    if (msg.content === 'ping') {
-        msg.reply('Pong!');
+discord.on('message', message => {
+    if (message.content === 'ping') {
+        message.reply('Pong!');
+    }
+    if (message.content.toLowerCase() === 'привет') {
+        message.channel.send(`привет, ${message.author.username}\nпривет`);
+    }
+    if (message.content.indexOf('/stats') === 0) {
+        handleStatsCommand(message);
     }
 });
 
@@ -67,3 +73,42 @@ api.get('/api/v1/process', async (context) => {
 app.use(api.routes());
 
 app.listen(HTTP_PORT, () => console.log(`Listening on port ${HTTP_PORT}!`));
+
+
+
+const request = require('request-promise')
+
+function handleStatsCommand(message) {
+    const param= message.content.replace('/stats', '').trim();
+    if (!param || param.indexOf('#') === -1) {
+        message.reply('необходимо указать BattleTag, например /stats oxiggy#2286');
+    } else {
+        const battleTag= param.split('#');
+        if (!battleTag[0] || !battleTag[1]) {
+            message.reply('неверный формат BattleTag');
+        } else {
+            request(`http://ow-api.herokuapp.com/profile/pc/eu/${battleTag[0]}-${battleTag[1]}'`)
+                .then(function (data) {
+                    try {
+                        data= JSON.parse(data)
+                        let message= ''
+                        message+= '' + data.username + ' (' + data.level + ' уровень, '+ data.competitive.rank +' ранг)\n'
+                        message+= '\n'
+                        message+= 'Быстрые игры:\n'
+                        message+= '— побед: '+ data.games.quickplay.won +'\n'
+                        message+= '— потрачено: '+ data.playtime.quickplay +'\n'
+                        message+= '\n'
+                        message+= 'Текущий сезон ранкеда:\n'
+                        message+= '— побед: '+ data.games.competitive.won +'\n'
+                        message+= '— поражений: '+ data.games.competitive.lost +'\n'
+                        message+= '— ничьих: '+ data.games.competitive.draw +'\n'
+                        message+= '— потрачено: '+ data.playtime.competitive +''
+                        message.reply(message);
+                    } catch (err) {
+                        // что-то пошло не так
+                    }
+                })
+            ;
+        }
+    }
+}
