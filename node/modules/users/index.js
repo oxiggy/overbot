5b2c73c2b1ceb43eb11785e4a3659736f3b5bfb3
Ada V <oxiggy@awsmtek.com>
2017-10-24 23:57:07 +0300

    список участников из файла

--- node/modules/users/index.js
+++ node/modules/users/index.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const request = require('request-promise');
+const USERS = require('../../../content/users/users.json');
 
 module.exports = function Users({ discord }) {
 
@@ -8,15 +8,30 @@ module.exports = function Users({ discord }) {
         const content = message.content.toLowerCase();
         if (content.indexOf('/users') === 0) {
             handleUsersCommand(message);
-            return
         }
-    })
+    });
 
     function handleUsersCommand(message) {
-        let content= ''
-        content+= 'Помощь по BattleTag:\n\n'
-        content+= 'oxiggy — oxiggy#2286\n'
+        let content= '';
+        content+= 'Помощь по BattleTag:\n\n';
+        Object.keys(USERS)
+            .sort(function(a, b) {
+                a= USERS[a].battleTag.toLowerCase();
+                b= USERS[b].battleTag.toLowerCase();
+                if (a < b) return -1;
+                if (a > b) return 1;
+                return 0
+            })
+            .forEach((userId) => {
+                const userProfile= discord.users.get(userId);
+                const userData= USERS[userId];
+                content+= userProfile.username;
+                content+= ' — ';
+                content+= userData.battleTag;
+                content+= '\n'
+            })
+        ;
         message.channel.send(content);
     }
 
-}
+};
