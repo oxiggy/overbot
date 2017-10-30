20007dd9338484f3a168eda9c10bc57f24a5f8cc
Ada V <oxiggy@awsmtek.com>
2017-10-31 01:36:14 +0300

    Бот задает вопросы

---
+++ node/modules/random-question/index.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const QUESTIONS = require('../../../content/questions/questions.json');
+
+module.exports = function RandomPic({ discord }) {
+
+    const activeQuestionByChannelId = {};
+
+    discord.on('message', message => {
+        if (message.author.id === discord.user.id) {
+            return
+        }
+        const activeQuestion = activeQuestionByChannelId[message.channel.id];
+        if (activeQuestion) {
+            if (checkAnswer(activeQuestion, message)) {
+                message.channel.send('да');
+            } else {
+                message.channel.send('не');
+            }
+            delete activeQuestionByChannelId[message.channel.id];
+        } else {
+            const content = message.content.toLowerCase();
+            if (content.indexOf('бот') >= 0 && content.indexOf('вопрос') >= 0) {
+                const i = getRandomInt(0, QUESTIONS.length - 1);
+                const question = QUESTIONS[i];
+                activeQuestionByChannelId[message.channel.id] = question;
+                message.channel.send(question.message);
+            }
+        }
+    });
+
+    function checkAnswer(question, message) {
+        if (question.answer === message.content.toLowerCase()) {
+            return true
+        }
+        return false
+    }
+
+    function getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+
+};
