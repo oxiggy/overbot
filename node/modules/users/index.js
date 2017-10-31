fc153bf5c877e0c50150cdb3595b9b569c6e97c0
Ada V <oxiggy@awsmtek.com>
2017-10-31 18:35:21 +0300

    Баланс может быть передан между участниками

--- node/modules/users/index.js
+++ node/modules/users/index.js
@@ -1,4 +1,5 @@
 'use strict';
+const fs= require('fs');
 
 const USERS = require('../../../content/users/users.json');
 
@@ -11,6 +12,12 @@ module.exports = function Users({ discord }) {
         }
         if (content.indexOf('бот') >= 0 && content.indexOf('баланс') >= 0) {
             handleUserBalance(message);
+        }
+        const giveBalanceRegexp= new RegExp('^бот,?\\s?(\\d+)\\s?нефти\\s?<@!?(\\S+)>');
+        if (giveBalanceRegexp.test(content)) {
+            const sourceUserId= message.author.id;
+            const [, amount, targetUserId]= giveBalanceRegexp.exec(content);
+            transferUserBalance(message, sourceUserId, targetUserId, amount);
         }
     });
 
@@ -43,6 +50,26 @@ module.exports = function Users({ discord }) {
             balance: 0,
         });
         message.reply(authorData.balance);
+    }
+
+    function transferUserBalance(message, sourceUserId, targetUserId, amount) {
+        amount= parseInt(''+amount, 10);
+        const sourceUserData= getUserData(sourceUserId);
+        const targetUserData= getUserData(targetUserId);
+        if (amount > sourceUserData.balance) {
+            message.reply(`у тебя столько нет`);
+            return
+        }
+        sourceUserData.balance-= amount;
+        targetUserData.balance+= amount;
+        message.channel.send(`<@${sourceUserId}> передает <@${targetUserId}> **${amount} нефти**`);
+        fs.writeFile('content/users/users.json', JSON.stringify(USERS, null, 2));
+    }
+
+    function getUserData(userId) {
+        return USERS[userId]= (USERS[userId] || {
+            balance: 0,
+        })
     }
 
 };
